ecd85cd42716ac9adabb5ae5a0d50d92e770c573
DaanKorver <daank2000@hotmail.nl>
2022-05-30 15:27:47 +0200

    updated projects

--- outputs.js
+++ outputs.js
@@ -23,7 +23,7 @@ const fetch = `
         ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
       ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
     ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            Daan Korver
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <a target="_blank" href="/">CV</a>
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <a target="_blank" href="https://daankorver.github.io/CV/">CV</a>
 ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <a target="_blank" href="/">Portfolio</a>
 ▐▓                                 ▐▓       -----------
 ▐▓          > DAAN KORVER          ▐▓        CONTACT 
@@ -60,23 +60,41 @@ id: 0
 Name: Catstagram
 Description: A instagram clone but only for cats (using AI to check that)
 Tags: HTML, CSS, JS, Sveltekit, TensorFlow
-Link: <a href="https://github.com/DaanKorver/catstagram">catstagram.xyz</a>
-Github: <a href="https://github.com/DaanKorver/catstagram">/catstagram</a>
+Link: <a target="_blank" href="https://github.com/DaanKorver/catstagram">Coming Soon...</a>
+Github: <a target="_blank" href="https://github.com/DaanKorver/catstagram">/catstagram</a>
 ==========================================================================
 id: 1
 Name: Scrollbook
 Description: Making reading books a lot better
 Tags: HTML, CSS, JS, GSAP, Nuxt
-Link: <a href="scrollbook.student.fdnd.nl">Scrollbook</a>
-Github: <a href="https://github.com/DaanKorver/the-startup-scrollbook">/the-startup-scrollbook</a>
+Link: <a target="_blank" href="https://scrollbook.student.fdnd.nl">Scrollbook</a>
+Github: <a target="_blank" href="https://github.com/DaanKorver/the-startup-scrollbook">/the-startup-scrollbook</a>
 ==========================================================================
-id: 2
-Name: Catstagram
-Description: A instagram clone but only for cats (using AI to check that)
-Tags: HTML, CSS, JS, Sveltekit, TensorFlow
-Link: <a href="https://github.com/DaanKorver/catstagram">catstagram.xyz</a>
-Github: <a href="https://github.com/DaanKorver/catstagram">/catstagram</a>
+id: 3
+Name: Borthon.nl
+Description: A webshop for my cousin. Work in progress, not finished yet
+Tags: HTML, CSS, JS, Sveltekit, Commerce.js
+Link: <a target="_blank" href="https://github.com/DaanKorver/borthon.nl">borton.nl</a>
+Github: <a target="_blank" href="https://github.com/DaanKorver/borthon.nl">/Private repository</a>
 ==========================================================================
+id: 4
+Name: Beyco Newspage
+Description: A concept news page for Beyco.nl
+Tags: HTML, CSS, JS, Sveltekit, Commerce.js
+Link: <a target="_blank" href="https://beycodk.student.fdnd.nl/">Beyco newspage</a>
+Github: <a target="_blank" href="https://github.com/DaanKorver/chippr-beyco-newspage">/chippr-beyco-newspage</a>
+==========================================================================
+id: 5
+Name: HighestCrit
+Description: World of Warcraft addon to track your highest crit.
+Tags: Lua, Scripting, WoW
+Link: <a target="_blank" href="https://www.curseforge.com/wow/addons/highestcrit">HighestCrit</a>
+Github: <a target="_blank" href="https://github.com/DaanKorver/HighestCrit">/HighestCrit</a>
+==========================================================================
+
+Want to see more?
+
+Checkout my <a target="_blank" href="https://github.com/DaanKorver">Github</a>
 `
 
 const about = `
